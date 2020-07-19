9f88a6ebe8181e78833f6509a28fb88cb5220d54
Willie Christiani <willie.christiani@gmail.com>
2020-07-19 15:23:36 +0200

    Code update Added RSSI data as it is needed by Brewfather

--- __init__.py
+++ __init__.py
@@ -12,66 +12,68 @@ blueprint = Blueprint('ispindel', __name__)
 cache = {}
 
 def calcGravity(polynom, tilt, unitsGravity):
-	if unitsGravity == "SG":
-		rounddec = 3
-	else:
-		rounddec = 2
+        if unitsGravity == "SG":
+                rounddec = 3
+        else:
+                rounddec = 2
 
-	# Calculate gravity from polynomial
-	tilt = float(tilt)
-	result = eval(polynom)
-	result = round(float(result),rounddec)
-	return result
+        # Calculate gravity from polynomial
+        tilt = float(tilt)
+        result = eval(polynom)
+        result = round(float(result),rounddec)
+        return result
 
 @cbpi.sensor
 class iSpindel(SensorActive):
-	key = Property.Text(label="iSpindel Name", configurable=True, description="Enter the name of your iSpindel")
-	sensorType = Property.Select("Data Type", options=["Temperature", "Gravity", "Battery"], description="Select which type of data to register for this sensor")
-	tuningPolynom = Property.Text(label="Tuning Polynomial", configurable=True, default_value="tilt", description="Enter your iSpindel polynomial. Use the variable tilt for the angle reading from iSpindel. Does not support ^ character.")
-	unitsGravity = Property.Select("Gravity Units", options=["SG", "Brix", "°P"], description="Displays gravity reading with this unit if the Data Type is set to Gravity. Does not convert between units, to do that modify your polynomial.")
+        key = Property.Text(label="iSpindel Name", configurable=True, description="Enter the name of your iSpindel")
+        sensorType = Property.Select("Data Type", options=["Temperature", "Gravity", "Battery", "Angle", "RSSI"], description="Select which type of data to register for this sensor")
+        tuningPolynom = Property.Text(label="Tuning Polynomial", configurable=True, default_value="tilt", description="Enter your iSpindel polynomial. Use the variable tilt for the angle reading from iSpindel. Does not support ^ character.")
+        unitsGravity = Property.Select("Gravity Units", options=["SG", "Brix", "°P"], description="Displays gravity reading with this unit if the Data Type is set to Gravity. Does not convert between units, to do that modify your polynomial.")
 
-	def get_unit(self):
-		if self.sensorType == "Temperature":
-			return "°C" if self.get_config_parameter("unit", "C") == "C" else "°F"
-		elif self.sensorType == "Gravity":
-			return self.unitsGravity
-		elif self.sensorType == "Battery":
-			return "V"
-		else:
-			return " "
+        def get_unit(self):
+                if self.sensorType == "Temperature":
+                        return "°C" if self.get_config_parameter("unit", "C") == "C" else "°F"
+                elif self.sensorType == "Gravity":
+                        return self.unitsGravity
+                elif self.sensorType == "Battery":
+                        return "V"
+                elif self.sensorType == "Angle":
+                        return "°"
+                else:
+                        return " "
 
-	def stop(self):
-		pass
+        def stop(self):
+                pass
 
-	def execute(self):
-		global cache
-		while self.is_running():
-			try:				
-				if cache[self.key] is not None:
-					if self.sensorType == "Gravity":
-						reading = calcGravity(self.tuningPolynom, cache[self.key]['Angle'], self.unitsGravity)
-					else:
-						reading = cache[self.key][self.sensorType]
-					self.data_received(reading)
-			except:
-				pass
-			self.api.socketio.sleep(1) 
+        def execute(self):
+                global cache
+                while self.is_running():
+                        try:
+                                if cache[self.key] is not None:
+                                        if self.sensorType == "Gravity":
+                                                reading = calcGravity(self.tuningPolynom, cache[self.key]['Angle'], self.unitsGravity)
+                                        else:
+                                                reading = cache[self.key][self.sensorType]
+                                        self.data_received(reading)
+                        except:
+                                pass
+                        self.api.socketio.sleep(1)
 
 @blueprint.route('/api/hydrometer/v1/data', methods=['POST'])
 def set_temp():
-	global cache
-	
-	data = request.get_json()
-	id = data["name"]
-	temp = round(float(data["temperature"]), 2)
-	angle = data["angle"]
-	battery = data["battery"]
+        global cache
 
-	cache[id] = {'Temperature': temp, 'Angle': angle, 'Battery': battery}
+        data = request.get_json()
+        id = data["name"]
+        temp = round(float(data["temperature"]), 2)
+        angle = data["angle"]
+        battery = round(float(data["battery"]), 2)
+        RSSI = data["RSSI"]
 
-	return ('', 204)
+        cache[id] = {'Temperature': temp, 'Angle': angle, 'Battery': battery, 'RSSI': RSSI}
+        return ('', 204)
 
 @cbpi.initalizer()
 def init(cbpi):
-	print "INITIALIZE ISPINDEL MODULE"
-	cbpi.app.register_blueprint(blueprint)
+        print "INITIALIZE ISPINDEL MODULE"
+        cbpi.app.register_blueprint(blueprint)
